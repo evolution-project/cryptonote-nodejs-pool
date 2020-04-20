94b54fcb0e7b08f844fea4bcd2d922dc21ed5b7a
evolution-project <evolutionproject@protonmail.com>
2020-04-21 00:04:03 +0300

    update

--- website_example/config.js
+++ website_example/config.js
@@ -1,5 +1,5 @@
-var api = "https://multi-miner.smartcoinpool.net:8119";
-let parentCoin = "Arqma"
+var api = "http://pool-host:8111";
+let parentCoin = "Evolution"
 
 var email = "support@poolhost.com";
 var telegram = "https://t.me/YourPool";
@@ -8,10 +8,10 @@ var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+var blockchainExplorer = "http://explorer.evolutionproject.space/{symbol}/block/{id}";
+var blockchainExplorerMerged = "http://e/?hash={id}#block";
+var transactionExplorer = "http://explorer.evolutionproject.space/{symbol}/transaction/{id}";
+var transactionExplorerMerged = "http://e/?hash={id}#transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
