2fbd37917d774260c2762e950cb3896d646886d6
Evolution-Project <evolutionproject@protonmail.com>
2021-03-22 12:28:28 +0200

    new colors, discord alerts added

--- html/config.js
+++ html/config.js
@@ -1,4 +1,4 @@
-var api = "http://pool-host:8111";
+var api = "http://bloody.go.ro:5555";
 let parentCoin = "Evolution"
 
 var email = "support@poolhost.com";
@@ -9,9 +9,12 @@ var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 var blockchainExplorer = "http://explorer.evolutionproject.space/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://e/?hash={id}#block";
 var transactionExplorer = "http://explorer.evolutionproject.space/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://e/?hash={id}#transaction";
+
+//var blockchainExplorerMerged = "http://e/?hash={id}#block";
+//var transactionExplorerMerged = "http://e/?hash={id}#transaction";
+
+
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
