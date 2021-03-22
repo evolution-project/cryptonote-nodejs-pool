8b5d3dadadb4d86e2e8bdf39ca8b4c8d029f7cb8
evolution-project <60467816+evolution-project@users.noreply.github.com>
2021-03-22 12:53:26 +0200

    Update config.js

--- html/config.js
+++ html/config.js
@@ -1,4 +1,4 @@
-var api = "http://bloody.go.ro:5555";
+var api = "poolhost.com:port | or | poolhost.com/api";
 let parentCoin = "Evolution"
 
 var email = "support@poolhost.com";
@@ -8,8 +8,8 @@ var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://explorer.evolutionproject.space/{symbol}/block/{id}";
-var transactionExplorer = "http://explorer.evolutionproject.space/{symbol}/transaction/{id}";
+var blockchainExplorer = "https://explorer.evolutionproject.space/{symbol}/block/{id}";
+var transactionExplorer = "https://explorer.evolutionproject.space/{symbol}/transaction/{id}";
 
 //var blockchainExplorerMerged = "http://e/?hash={id}#block";
 //var transactionExplorerMerged = "http://e/?hash={id}#transaction";
